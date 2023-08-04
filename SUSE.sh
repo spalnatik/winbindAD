70bc60a828830fe6997587f7b5f70e0cb57618d9
spalnatik <139609488+spalnatik@users.noreply.github.com>
2023-08-04 21:42:58 +0530

    Update SUSE.sh

--- SUSE.sh
+++ SUSE.sh
@@ -100,7 +100,7 @@ hostname=`hostname`
 
 hostnamectl set-hostname $hostname.intl.contoso.com
 
-echo "10.0.0.5        $hostname.intl.contoso.com $hostname" >> /etc/hosts
+echo "10.0.0.8        $hostname.intl.contoso.com $hostname" >> /etc/hosts
 
 #echo "$domain_admin_password" | kinit $domain_admin_username
 echo "$2" | kinit $1
