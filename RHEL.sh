ff4cf2ab0d8bca1b6c0bc01267323a1f0ca7fbc8
spalnatik <139609488+spalnatik@users.noreply.github.com>
2023-08-04 22:23:48 +0530

    Update RHEL.sh

--- RHEL.sh
+++ RHEL.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
 
+
+sudo update-crypto-policies --set DEFAULT:AD-SUPPORT
+
+sudo yum install --disablerepo='*' --enablerepo='*microsoft*' 'rhui-azure-* -y 
+
+sudo yum install realmd oddjob-mkhomedir oddjob samba-winbind-clients samba-winbind samba-common-tools samba-winbind-krb5-locator -y
+
+sudo  yum install samba -y
+
+mv /etc/samba/smb.conf /etc/samba/smb.conf.bak
+
+realm join --membership-software=samba --client-software=winbind intl.contoso.com -U tempadmin
