69f9f0f8d58de4868eacd32b8f8a4947d9ab0c1a
spalnatik <139609488+spalnatik@users.noreply.github.com>
2023-08-06 18:09:17 +0530

    Update RHEL.sh

--- RHEL.sh
+++ RHEL.sh
@@ -2,7 +2,7 @@
 
 sudo echo 'append domain-search "intl.contoso.com"' >> /etc/dhcp/dhclient.conf
 sudo sed -i '/^\[main\]/a dhcp = dhclient' /etc/NetworkManager/NetworkManager.conf
-echo "nameserver 10.0.0.15" >> /etc/resolv.conf
+#echo "nameserver 10.0.0.15" >> /etc/resolv.conf
 sudo systemctl restart NetworkManager
 
 sudo update-crypto-policies --set DEFAULT:AD-SUPPORT
