edfe282a1137572723d5204664689b5dcca66819
spalnatik <139609488+spalnatik@users.noreply.github.com>
2023-08-07 15:04:16 +0530

    Update RHEL.sh

--- RHEL.sh
+++ RHEL.sh
@@ -1,7 +1,8 @@
-
-
 #!/bin/bash
-
+sudo echo 'append domain-search "intl.contoso.com"' >> /etc/dhcp/dhclient.conf
+sudo sed -i '/^\[main\]/a dhcp = dhclient' /etc/NetworkManager/NetworkManager.conf
+#echo "nameserver 10.0.0.15" | sudo tee /etc/resolv.conf
+sudo systemctl restart NetworkManager
 
 cp -p /etc/krb5.conf /etc/krb5.conf_bkp
 
