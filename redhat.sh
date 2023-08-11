aeb70906d59c9068569ac0edd264f0d4d9c63206
spalnatik <139609488+spalnatik@users.noreply.github.com>
2023-08-11 22:52:50 +0530

    Update redhat.sh

--- redhat.sh
+++ redhat.sh
@@ -47,6 +47,6 @@ hostnamectl set-hostname $hostname.lab.local
 ip=`hostname -I | awk '{print $1}'`
 
 echo "$ip        $hostname.lab.local $hostname" >> /etc/hosts
-
+echo "%lab.local\\$domain_admin_username ALL=(ALL)    NOPASSWD:ALL"
 
 net ads join -k
